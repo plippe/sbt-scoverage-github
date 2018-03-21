d0ed7d1123785ede0c102a0ccd89f7b4cac3e7a2
Philippe Vinchon <pvinchon@gilt.com>
2018-03-21 17:34:57 +0000

    Run method outside of plugin code, main example

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,50 @@
+package com.github.plippe
+
+import cats._
+import cats.effect._
+import cats.implicits._
+import java.io.File
+import java.util.NoSuchElementException
+import org.http4s.client.blaze.Http1Client
+import scala.concurrent.ExecutionContext
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import com.github.plippe.github._
+
+object Main {
+    def main(args: Array[String]): Unit = {
+        val namedFiles = Map(
+            "A" -> new File("/Users/pvinchon/Code/sbt-scoverage-github/src/test/resources/scoverage.a.xml"),
+            "B" -> new File("/Users/pvinchon/Code/sbt-scoverage-github/src/test/resources/scoverage.b.xml"),
+            "C" -> new File("/Users/pvinchon/Code/sbt-scoverage-github/src/test/resources/scoverage.empty.xml")
+        )
+
+        val gitHubToken = System.getenv("GITHUB_TOKEN")
+
+        run[IO](namedFiles, gitHubToken).unsafeRunSync
+    }
+
+    def run[F[_]](namedFiles: Map[String, File], gitHubToken: String)(implicit ec: ExecutionContext, M: MonadError[F, Throwable], E: Effect[F]): F[Boolean] = {
+        for {
+            namedCoverages <- namedFiles.toList
+                .map { case (n, f) => ScoverageXmlReader.read(f).map { c => NamedCoverage(n, c) } }
+                .traverse(M.fromEither)
+
+            report = NamedCoverages.render(namedCoverages.toArray, Comment.maxWidth)
+            comment = Comment(report)
+
+            gitRemoteUrl <- M.fromEither(Git.remoteUrl("origin"))
+            gitHeadSha <- M.fromEither(Git.headSha)
+
+            http4sClient <- Http1Client[F]()
+            httpClient = HttpClientHttp4s[F](http4sClient)
+            gitHubClient =  GitHubClientCirce[F](httpClient, gitHubToken)
+
+            gitHubPullRequests <- gitHubClient.listPullRequests(gitRemoteUrl.owner, gitRemoteUrl.repository)
+            gitHubPullRequest <- gitHubPullRequests.find(_.head.sha == gitHeadSha)
+                .fold(M.raiseError[PullRequest](new NoSuchElementException(s"No pull request with head sha $gitHeadSha")))(M.pure)
+
+            _ <- gitHubClient.postComment(gitRemoteUrl.owner, gitRemoteUrl.repository, gitHubPullRequest.number, comment)
+        } yield true
+    }
+}
